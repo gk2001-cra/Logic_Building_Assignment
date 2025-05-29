22fd967f3a2689249306b396b8cfd939c0833e62
Gaurav Kumbhar <79054834+gk2001-cra@users.noreply.github.com>
2025-05-29 10:33:17 +0530

    Add files via upload

---
+++ Assignment_16/Ass16_3.c
@@ -0,0 +1,62 @@
+// Input :  Rows = 4, Columns = 4 
+/*
+    $       *       *       *
+    #       $       *       *
+    #       #       $       *
+    #       #       #       $
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input");
+        return;
+    }
+    
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++) 
+        {  
+            if(i == j)
+            {
+                printf("$\t"); 
+            }
+
+            else if(i > j)
+            {
+                printf("#\t");
+            }
+
+            else
+            {
+                printf("*\t");
+            }
+
+        }
+
+    printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter Number Of Rows : ");
+    scanf("%d",& iValue1);
+
+    printf("Enter Number Of Columns : ");
+    scanf("%d",& iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
